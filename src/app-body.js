53fe916a519eed4bf7868c2fc0b89ab60327b70f
HeD96 <hedplanet96@gmail.com>
2023-11-08 22:08:55 +0300

    added menu panel and new task form

--- src/app-body.js
+++ src/app-body.js
@@ -14,8 +14,40 @@ export function AppBodyRender() {
     const taskSection = document.createElement("div");
     taskSection.classList.add("task-section");
 
+    const taskSectionTitle = document.createElement("h1");
+    taskSectionTitle.textContent = "Tasks";
+
+    const addTaskButton = document.createElement("button");
+    taskSectionTitle.append(addTaskButton);
+    addTaskButton.classList.add("add-task");
+    addTaskButton.textContent = "+";
+
+    const todayTasks = document.createElement("p");
+    todayTasks.classList.add("today-tasks");
+    todayTasks.textContent = "Today";
+
+    const upcomingTasks = document.createElement("p");
+    upcomingTasks.classList.add("upcoming-tasks");
+    upcomingTasks.textContent = "Upcoming";
+
+    const completedTasks = document.createElement("p");
+    completedTasks.classList.add("completed-tasks");
+    completedTasks.textContent = "Completed";
+
+    taskSection.append(
+        taskSectionTitle,
+        todayTasks,
+        upcomingTasks,
+        completedTasks
+    );
+
     const projectSection = document.createElement("div");
     projectSection.classList.add("project-section");
+
+    const projectSectionTitle = document.createElement("h1");
+    projectSectionTitle.textContent = "My Projects";
+
+    projectSection.append(projectSectionTitle);
 
     menuPanel.append(profile, taskSection, projectSection);
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import "./style.css";
 import { AppBodyRender } from "./app-body.js";
+import { NewTaskForm } from "./newTaskForm.js";
 
 AppBodyRender();
+
+const addNewTask = document.querySelector(".add-task");
+addNewTask.addEventListener("click", NewTaskForm);
